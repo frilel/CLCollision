0f3c769ed9a709b5e8401b6cc84bba3d966f94f2
frilel <crillefrille@gmail.com>
2022-05-05 23:03:26 +0200

    Initial commit

---
+++ GJK.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace CLCollision
+{
+    public class GJK
+    {
+        public int c;
+
+        public void AddValues(int a, int b)
+        {
+            c = a + b;
+        }
+
+        public static int GenerateRandom(int min, int max)
+        {
+            System.Random rand = new System.Random();
+            return rand.Next(min, max);
+        }
+
+    }
+}
